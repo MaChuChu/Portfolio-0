839972a22b13e59e2aaa45bbacfc4e7d7ee71eb6
Ranjeth Ravichandran <ranjethravichandran@hotmail.com>
2023-02-24 02:17:02 +0000

    Completed Portfolio

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+// Typing Animation
+var typed = new Typed(".typing", {
+    strings:["Programmer", "Web Designer", "Web Developer", "Software Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+});
