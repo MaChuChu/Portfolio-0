889b4dc51182fc9d28a7e82d0d1e07287a65796e
Ranjeth Ravichandran <ranjethravichandran@hotmail.com>
2023-02-24 03:45:04 +0000

    Fixed Typing

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 
 // Typing Animation
 var typed = new Typed(".typing", {
-    strings:["Programmer", "Web Designer", "Web Developer", "Software Developer"],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop:true
+    strings:["Programmer", "","Web Designer", "", "Web Developer", "", "Software Developer", ""],
+    typeSpeed: 80,
+    backSpeed: 50,
+    backDelay: 500,
+    loop: true
 });
